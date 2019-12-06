a264fc79377ca27ed96569da2d8f4ae46b840a88
zhangjie <zhangjie@11wlw.com>
2019-12-06 16:32:25 +0800

    add: test

---
+++ lib/wxToken/tests/locationSearch.test.ts
@@ -0,0 +1,19 @@
+import LocationSearch from "../src/LocationSearch";
+
+const testData =
+  "?appid=appidappid&redirect_uri=http://baidu.com&response_type=code&scope=snsapi_base&agentid=agentId&state=STATE#wechat_redirect";
+
+const expectData = {
+  appid: "appidappid",
+  redirect_uri: "http://baidu.com",
+  response_type: "code",
+  scope: "snsapi_base",
+  agentid: "agentId",
+  state: "STATE#wechat_redirect"
+};
+
+describe("my-utils: LocationSearch", () => {
+  test("LocationSearch(): true", () => {
+    expect(LocationSearch(testData)).toMatchObject(expectData);
+  });
+});
